9474ff3e081293de9cfe83a153d801136bba17a4
haya14busa <hayabusa1419@gmail.com>
2013-12-16 23:43:59 +0900

    Add font-size style and meta viewport

--- JavaScript/MarkdownQuote.js
+++ JavaScript/MarkdownQuote.js
@@ -5,11 +5,14 @@ javascript:(function() {
     QLink="> -- <cite>["+T+"]"+"("+L+")";
     Ptext=QText+"\r\r"+QLink;
     TextUrl="data:text/html;charset=UTF-8,
+    <meta name='viewport' content='width=device-width user-scalable=no, initial-scale=1.0, minimum-scale=1.0'>
+    <script src=http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js></script>
     <style>
     textarea{
         width:100%;
         height:100%;
         border:none;
+        font-size:22px;
     }
     </style>
     <textarea onclick='this.select();'>"+ Ptext +"</textarea>";
