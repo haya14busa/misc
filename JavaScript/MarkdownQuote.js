0b6c4d167092ebc00af4cfe250ce1a2f9f92d1b7
haya14busa <hayabusa1419@gmail.com>
2013-12-16 22:39:25 +0900

    Add MarkdownQuote

---
+++ JavaScript/MarkdownQuote.js
@@ -0,0 +1,11 @@
+javascript:(function() {
+    L=document.location.href;
+    T=document.title;
+    QText="> "+encodeURIComponent(document.getSelection());
+    QLink="> -- <cite>["+T+"]"+"("+L+")";
+    Ptext=QText+"\r\r"+QLink;
+    TextUrl="data:text/html;charset=UTF-8,
+    <textarea>"+ Ptext +"</textarea>";
+    window.open(TextUrl,"_blank");void(0);
+}
+)()
