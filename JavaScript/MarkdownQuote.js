0a3a3ef2786f86049c7544daa74857ae64a2e443
haya14busa <hayabusa1419@gmail.com>
2013-12-16 22:49:36 +0900

    Apply style

--- JavaScript/MarkdownQuote.js
+++ JavaScript/MarkdownQuote.js
@@ -5,6 +5,13 @@ javascript:(function() {
     QLink="> -- <cite>["+T+"]"+"("+L+")";
     Ptext=QText+"\r\r"+QLink;
     TextUrl="data:text/html;charset=UTF-8,
+    <style>
+    textarea{
+        width:100%;
+        height:100%;
+        border:none;
+    }
+    </style>
     <textarea onclick='this.select();'>"+ Ptext +"</textarea>";
     window.open(TextUrl,"_blank");void(0);
 }
